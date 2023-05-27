69995e468d12584a8c5b85fe364984d31d7cfb43
devlopersabbir <devlopersabbir@gmail.com>
2023-05-28 04:15:58 +0600

    notification setup done

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { Container, Heading } from "@chakra-ui/react";
 
 const App = () => {
+  chrome.runtime.sendMessage({ action: "fetchNotifications" });
+
   return (
     <Container w="400px" h="auto" mx="auto" p={4}>
       <Heading>Hello</Heading>
